4cf67df17f4ddf28646aece0192e7828c089259d
tsumazarin <69566581+tsumazarin@users.noreply.github.com>
2020-08-18 12:11:32 +0900

    first commit

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,17 @@
 <?php
   //データベース接続
     try{
-      $db=new PDO('mysql:dbname=book;host=localhost;charset=utf8', 'root', 'root');
+      $dbi = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+  $dbi['dbname'] = ltrim($dbi['path'], '/');
+  $dsn = "mysql:host={$dbi['host']};dbname={$dbi['dbname']};charset=utf8";
+  $user = $dbi['user'];
+  $password = $dbi['pass'];
+  $options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+  );
+  $db = new PDO($dsn,$user,$password,$options);
     }catch(PDOException $e){
       echo "DB接続エラー：{$e->getMessage()}";
     }
