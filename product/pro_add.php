3b8db795ef39f7717323d62fc9fb135b9a07a77e
tsumazarin <69566581+tsumazarin@users.noreply.github.com>
2020-08-30 21:48:05 +0900

    third commit

--- product/pro_add.php
+++ product/pro_add.php
@@ -29,7 +29,7 @@
 
     $image = $_FILES['image'];
     $ext = substr($image['name'], -3);
-    if ($image['size'] <= 0) {
+    if (empty($image['name'])) {
       $error['image'] = 'blank';
     }elseif ($image['size'] > 10000000) {
       $error['image'] = 'size';
